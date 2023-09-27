2cde66b4677559cc11287e8732eaa1af71953103
Daniel-Cairney <114543733+Daniel-Cairney@users.noreply.github.com>
2023-09-28 03:04:02 +1000

    fixed player spawns

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class DoorTrigger : MonoBehaviour
 {
-    // Public variable to specify the scene to load
+    // Public variables to specify the scene to load and the spawn position
     public string sceneToLoad;
+    public Vector2 spawnPosition;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Enter Door");
+
+        // Save the desired spawn position in a PlayerPrefs or another method of your choice
+        PlayerPrefs.SetFloat("SpawnX", spawnPosition.x);
+        PlayerPrefs.SetFloat("SpawnY", spawnPosition.y);
 
         // Load the specified scene
         SceneManager.LoadScene(sceneToLoad);
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,25 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rb= GetComponent<Rigidbody2D>();
+        if (PlayerPrefs.HasKey("SpawnX") && PlayerPrefs.HasKey("SpawnY"))
+        {
+            // Retrieve the spawn coordinates
+            float spawnX = PlayerPrefs.GetFloat("SpawnX");
+            float spawnY = PlayerPrefs.GetFloat("SpawnY");
+
+            // Set the player's position to the specified spawn coordinates
+            transform.position = new Vector2(spawnX, spawnY);
+
+            // Remove the saved spawn coordinates to avoid using them again
+            PlayerPrefs.DeleteKey("SpawnX");
+            PlayerPrefs.DeleteKey("SpawnY");
+        }
+        else
+        {
+            Debug.Log("No spawn coordinates found. Using default spawn.");
+        }
     }
+
 
     private void Update()
     {
